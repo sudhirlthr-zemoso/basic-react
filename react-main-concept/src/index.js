c2ad558d7dca93f85ab2c9fdda824660e1e931b7
sudhir <sudhirlthr@gmail.com>
2020-05-19 19:38:03 +0530

    Redering React element which is immutable

--- react-main-concept/src/index.js
+++ react-main-concept/src/index.js
@@ -28,10 +28,19 @@ const user = {
 };
 
 const element = <h1>Hello , {formatName(user)}</h1>;
+function tick() {
+    const element = (
+        <div>
+            <h2>Hello World</h2>
+            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+        </div>
+    );
+    ReactDOM.render(element, document.getElementById('root'));
+}
 
-
+setInterval(tick, 1000);
 ReactDOM.render(
-  greeting(user),
+  setInterval,
   document.getElementById('root')
 );
 
